e7b3ed2b7bf9ac569daa273d264c43dd218712f9
sebastheonesalomon-max <sebastheonesalomon@icloud.com>
2026-05-03 16:35:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,4 @@ def home():
 
     return render_template("index.html", response=response)
 
-if __name__ == "__main__":
-    import os
 
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
